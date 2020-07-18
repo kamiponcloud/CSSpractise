6b737ac1606f8eb0db417f78f67d4ce805f90e00
kamiponcloud <1906219821@qq.com>
2020-07-18 23:41:43 +0800

    pornhub

---
+++ js/pornhub.js
@@ -0,0 +1,8 @@
+function creatpornhub() {
+    let p = document.getElementById("porn").value;
+    let h = document.getElementById("hub").value;
+    let p1 = document.getElementById("porn1");
+    let h1 = document.getElementById("hub1");
+    p1.innerText = p;
+    h1.innerText = h;
+}
